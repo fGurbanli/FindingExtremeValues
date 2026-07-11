efea5c2dd9f0acde913232dd0f85096bb53a4755
Furgan Gurbanli <f.qurbanli.2006@gmail.com>
2026-07-11 21:59:09 +0400

    Finding positions

--- main.c
+++ main.c
@@ -7,22 +7,20 @@ int FindMinimumValue(int num[], int len);
 
 int main(void)
 {
-
     int length = 6;
     int numbers[length];
 
     ReadIntArray(numbers, length);
     PrintArray(numbers, length);
-    int MaxVal = FindMaximumValue(numbers, length);
-    int MinVal = FindMinimumValue(numbers, length);
-    printf("\nMaximum value is: %d\n", MaxVal);
-    printf("Minimum value is: %d\n", MinVal);
+    FindMaximumValue(numbers, length);
+    FindMinimumValue(numbers, length);
+
     return 0;
 }
 
 void ReadIntArray(int nums[], int len)
 {
-    for (int i = 1; i < len; i++)
+    for (int i = 0; i < len; i++)
     {
         printf("Enter %d / %d: ", i + 1 , len);
         scanf("%d", &nums[i]);
@@ -39,13 +37,25 @@ void PrintArray(int nums[], int len)
 
 int FindMaximumValue(int num[], int len)
 {
+    int maxCount = 1;
     int maxVal = num[0];
     for(int i = 1; i < len; i++)
     {
         if (num[i] > maxVal)
         {
-           maxVal = num[i];
+            maxVal = num[i];
+            maxCount = 1; //resetting count
         }
+        else if (num[i] == maxVal) maxCount++;
+    }
+
+    printf("\nMaximum value is: %d", maxVal);
+    printf("\nMaximum value occurred %d times", maxCount);
+    printf("\nMaximum value positions: ");
+
+    for (int i = 0; i < len; i++)
+    {
+        if (num[i] == maxVal) printf("%d ", i + 1);
     }
     return maxVal;
 }
@@ -53,12 +63,24 @@ int FindMaximumValue(int num[], int len)
 int FindMinimumValue(int num[], int len)
 {
     int minVal = num[0];
-    for(int i = 0; i < len; i++)
+    int minCount = 1;
+    for(int i = 1; i < len; i++)
     {
         if (num[i] < minVal)
         {
             minVal = num[i];
+            minCount = 1; //resetting count
         }
+        else if (num[i] == minVal) minCount++;
+    }
+    printf("\n\nMinimum value is: %d", minVal);
+    printf("\nMinimum value occurred %d times", minCount);
+    printf("\nMinimum value positions: ");
+    for (int i = 0; i < len; i++)
+    {
+        if (num[i] == minVal) printf("%d ", i + 1);
     }
     return minVal;
 }
+
+
